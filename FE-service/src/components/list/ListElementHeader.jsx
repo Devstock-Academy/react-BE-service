b3c7c9fe9c170a0bc165828dda7895c07d5d922c
Bartlomiej Loza <bekloza-contractor@endeavorco.com>
2024-04-22 06:34:33 +0200

    after livecoding

--- FE-service/src/components/list/ListElementHeader.jsx
+++ FE-service/src/components/list/ListElementHeader.jsx
@@ -1,26 +1,35 @@
-import { useContext } from 'react'
-import style from './List.module.css'
-import Button from '../button/Button'
-import { OpenEyeIcon, ClosedEyeIcon } from '../../icons'
-import { LoginContext } from '../../context/LoginContext'
+import { createPortal } from "react-dom";
+import { useContext, useState } from "react";
+import style from "./List.module.css";
+import Button from "../button/Button";
+import { OpenEyeIcon, ClosedEyeIcon } from "../../icons";
+import { LoginContext } from "../../context/LoginContext";
+import Modal from "../modal/Modal";
 
 const ListElementHeader = ({ description, isAmountShow, setIsAmountShow }) => {
-  const { isLoggedIn } = useContext(LoginContext)
+  const { isLoggedIn } = useContext(LoginContext);
+  const [isModalShown, setIsModalShown] = useState(false);
 
   const handleClick = () => {
-    isLoggedIn
-      ? setIsAmountShow((prev) => !prev)
-      : alert('Podgląd tylko dla zalogowanych użytkowników')
-  }
+    isLoggedIn ? setIsAmountShow((prev) => !prev) : setIsModalShown(true);
+  };
+
+  const modal = createPortal(
+    <Modal variant="secondary" onClose={() => setIsModalShown(false)}>
+      Podgląd tylko dla zalogowanych userów
+    </Modal>,
+    document.body
+  );
 
   return (
-    <div className={style.cardHeader}>
+    <div className={style.cardHeader} onClick={() => alert("propagacja")}>
       <div className={style.description}>{description}</div>
       <Button iconButton onClick={handleClick}>
         {!isAmountShow ? <ClosedEyeIcon /> : <OpenEyeIcon />}
       </Button>
+      {isModalShown && modal}
     </div>
-  )
-}
+  );
+};
 
-export default ListElementHeader
+export default ListElementHeader;
