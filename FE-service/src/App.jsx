8b82860f24c94b0b9f523630177bd2ec55789d1c
Bartlomiej Loza <bekloza-contractor@endeavorco.com>
2024-04-22 03:39:47 +0200

    after livecoding

--- FE-service/src/App.jsx
+++ FE-service/src/App.jsx
@@ -1,12 +1,16 @@
-import { Header, List } from './components'
-
+import { Button, Header, List } from "./components";
+import { useState } from "react";
 function App() {
+  const [showList, setShowList] = useState(true);
   return (
     <>
       <Header>Budget tracker</Header>
-      <List />
+      <Button onClick={() => setShowList((prev) => !prev)}>
+        Show/Hide List
+      </Button>
+      {showList && <List />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- FE-service/src/components/list/List.jsx
+++ FE-service/src/components/list/List.jsx
@@ -1,31 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import { countBalance } from '../../utils/balance'
-import { Select } from '../../components'
-import ListElement from './ListElement'
-import styles from './List.module.css'
+import { countBalance } from "../../utils/balance";
+import { Select } from "../../components";
+import ListElement from "./ListElement";
+import styles from "./List.module.css";
 
 const options = [
-  { value: '', label: 'Wszystko' },
-  { value: 'expense', label: 'Wydatki' },
-  { value: 'income', label: 'Przychody' },
-]
+  { value: "", label: "Wszystko" },
+  { value: "expense", label: "Wydatki" },
+  { value: "income", label: "Przychody" },
+];
 
-const url = 'http://localhost:3000/transactions'
+const url = "http://localhost:3000/transactions";
 
 const List = () => {
-  const [selectedValue, setSelectedValue] = useState('')
-  const [data, setData] = useState(null)
+  const [selectedValue, setSelectedValue] = useState("");
+  const [data, setData] = useState(null);
 
   const handleSelectChange = (newValue) => {
-    setSelectedValue(newValue)
-  }
+    setSelectedValue(newValue);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log("resize window");
+    };
+    alert("mount component");
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      alert("unmount");
+    };
+  }, []);
 
   useEffect(() => {
     fetch(`${url}/${selectedValue && `?type=${selectedValue}`}`)
       .then((response) => response.json())
-      .then((data) => setData(data))
-  }, [selectedValue])
+      .then((data) => setData(data));
+  }, [selectedValue]);
 
   return (
     <div className={styles.list}>
@@ -41,7 +54,7 @@ const List = () => {
         <ListElement listElement={item} key={item.id} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
