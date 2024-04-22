21422a5770bbf3815ea70354878ce9e1681357ff
Bartlomiej Loza <bekloza-contractor@endeavorco.com>
2024-04-22 03:18:12 +0200

    after live coding

--- FE-service/src/components/list/List.jsx
+++ FE-service/src/components/list/List.jsx
@@ -1,53 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { countBalance } from '../../utils/balance'
 import ListElement from './ListElement'
 import styles from './List.module.css'
 import Select from '../select/Select'
+import Button from '../button/Button'
 
-const transaction = [
-  {
-    id: 1,
-    categoryId: 1,
-    type: 'expense',
-    amount: 100,
-    date: '2024-04-01',
-    description: 'Weekly groceries',
-  },
-  {
-    id: 2,
-    categoryId: 2,
-    type: 'income',
-    amount: 2000,
-    date: '2024-04-01',
-    description: 'Monthly salary',
-  },
-]
+// const transaction = [
+//   {
+//     id: 1,
+//     categoryId: 1,
+//     type: 'expense',
+//     amount: 100,
+//     date: '2024-04-01',
+//     description: 'Weekly groceries',
+//   },
+//   {
+//     id: 2,
+//     categoryId: 2,
+//     type: 'income',
+//     amount: 2000,
+//     date: '2024-04-01',
+//     description: 'Monthly salary',
+//   },
+// ]
 
 const options = [
   { value: '', label: 'Wszystko' },
-  { value: 'expanse', label: 'Wydatki' },
+  { value: 'expense', label: 'Wydatki' },
   { value: 'income', label: 'Przychody' },
 ]
 
+const BASE_URL = 'http://localhost:3000'
+
 const List = () => {
   const [selectedValue, setSelectedValue] = useState('')
+  const [data, setData] = useState([])
 
   const handleSelectChange = (newValue) => {
     setSelectedValue(newValue)
   }
 
+  useEffect(() => {
+    fetchData()
+  }, [selectedValue])
+
+  const fetchData = () => {
+    fetch(`${BASE_URL}/transactions${selectedValue && `?type=${selectedValue}`}`)
+      .then(response => response.json())
+      .then(data => setData(data))
+  }
+
   return (
     <div className={styles.list}>
       <div className={styles.listHeader}>
-        <h2>Balance: {countBalance(transaction)} $</h2>
+        {/* <Button onClick={fetchData}>Pobierz dane</Button> */}
+        <h2>Balance: {countBalance(data)} $</h2>
         <Select
           options={options}
           onChange={handleSelectChange}
           value={selectedValue}
         />
       </div>
-      {transaction.map((item) => (
+      {data.map((item) => (
         <ListElement listElement={item} key={item.id} />
       ))}
     </div>
