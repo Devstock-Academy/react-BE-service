b730ef1db1a7e819c5095769a269c9ed63f4e6e4
Bartlomiej Loza <bekloza-contractor@endeavorco.com>
2024-05-14 11:14:15 +0200

    after livecoding

--- FE-service/src/main.jsx
+++ FE-service/src/main.jsx
@@ -1,13 +1,30 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { LoginProvider } from './context/LoginContext'
-import { NotificationProvider } from './context/NotificationContext'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { LoginProvider } from "./context/LoginContext";
+import { NotificationProvider } from "./context/NotificationContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { List, Organizer } from "./components/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: "/",
+  },
+  {
+    element: <List />,
+    path: "summary",
+  },
+  {
+    element: <Organizer />,
+    path: "organizer",
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <NotificationProvider>
     <LoginProvider>
-      <App />
+      <RouterProvider router={router} />
     </LoginProvider>
   </NotificationProvider>
-)
+);
